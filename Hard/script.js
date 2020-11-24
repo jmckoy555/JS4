a92b38ec6cd54cafffb2a511b8aabc2ba434baa6
Jordan McKoy <jordanmckoy@MacBook-Pro.local>
2020-11-23 23:07:13 -0500

    final commit

---
+++ Hard/script.js
@@ -0,0 +1,11 @@
+// Do Stuff Here
+window.updateTheBackground = function (className) {
+
+}
+
+function changeBG() {
+    document.body.style.backgroundColor = "red";
+}
+function changeBG2() {
+    document.body.style.backgroundColor = "white";
+}
